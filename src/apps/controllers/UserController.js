5a414753c6bf8f65128d5c0d59968834e617b117
Bruna <brunasilv126@gmail.com>
2023-07-01 00:03:40 -0300

    user delete

--- src/apps/controllers/UserController.js
+++ src/apps/controllers/UserController.js
@@ -73,5 +73,25 @@ class UserController {
         )
         return res.status(200).json({ message: "User updated!" });
      }
+
+     async delete(req, res) {
+        const userToDelete = await Users.findOne({
+            where: {
+                id: req.userId,
+            },
+        });
+
+        if (!userToDelete) {
+            return res.status(400).json({ message: "User not exists!"});
+        }
+
+        await Users.destroy({
+            where: {
+                id: req.userId,
+            },
+        });
+
+        returnres.status(200).json({ message: 'User deleted!'});
+     }
 }
 module.exports = new UserController();
