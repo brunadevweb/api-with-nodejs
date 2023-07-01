c054a836d61156d78df7c334ce716963e5d97df2
Bruna <brunasilv126@gmail.com>
2023-06-30 23:52:16 -0300

    user update

--- src/apps/controllers/UserController.js
+++ src/apps/controllers/UserController.js
@@ -1,3 +1,4 @@
+const bcryptjs = require('bcryptjs');
 const Users= require('../models/Users');
 
 class UserController {
@@ -19,5 +20,58 @@ class UserController {
         return res.send({ message: 'User created!' });
      }
 
+     async update(req, res) {
+        const {
+          name, avatar, bio, gender, old_password, new_password, confire_new_password, 
+        } = req.body;
+
+        const user = await Users.findOne({
+            where: {
+                id: req.userId,
+            },
+        });
+
+        if (!user) {
+            return res.status(400).json({message: "User not exits!"});
+        }
+
+        const encryptPassword = '';
+
+        if (old_password) {
+            if (!await user.checkPassword(old_password)) {
+                return res.status(401).json({ error: 'Old password does not match'});
+            }
+
+            if (!new_password  || !confirm_new_password) {
+                return res.status(401).json({
+                    error:'We need a new_password and confirm_new_password atributes!',
+                });
+            }
+
+            if (new_password != confirm_new_password) {
+                return res.status(401).json({
+                    error: "New password and confirm new password does not match!",
+                });
+            }
+            
+            encryptPassword = await bcryptjs.hash(new_password);
+        } 
+        await Users.update (
+            {
+                name: name || user.name,
+                avatar: avatar || user.avatar,
+                bio: bio || user.bio,
+                gender: gender || user.gender,
+                password_hash: encryptPassword || user.password_hash,
+            },
+            {
+                where: {
+                    id: user.id,
+                },
+            },
+
+        )
+        return res.status(200).json({ message: "User updated!" });
+     }
 }
 module.exports = new UserController();
